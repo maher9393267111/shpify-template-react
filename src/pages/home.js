d6d8ac173b20bb2d989e1e1eb0b8829e7ea96d6c
maher <gomemahero.com>
2022-05-29 12:47:12 +0300

    change  desk navbar to mobile navbar when  screen  resize

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,6 @@
 import Button from '@mui/material/Button';
 
-import * as React from 'react';
+import {useState,useEffect} from 'react';
 import Nav from '../components/nav';
 import Mobilenav from '../components/mobilenav';
 
@@ -12,6 +12,34 @@ import Mobilenav from '../components/mobilenav';
 
 
 const Home = () => {
+
+const [currentnav,setcurrentnav] = useState(<Nav/>);
+
+
+// change curent nav to mobile nav when screen is less than 600px
+useEffect(()=>{
+  
+
+
+window.addEventListener('resize',()=>{
+
+    if(window.innerWidth<888){
+        setcurrentnav(<Mobilenav/>);
+        console.log("mobile nav");
+        console.log(window.innerWidth);
+    }
+    else{
+        setcurrentnav(<Nav/>);
+        console.log("desktop nav");
+        console.log(window.innerWidth);
+    }
+
+
+})
+},[]);
+
+
+
     return (
         <div>
 
@@ -20,7 +48,7 @@ const Home = () => {
 <div>
 
 
-<Nav/>
+{currentnav}
 
 
 
